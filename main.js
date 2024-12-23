422f6de58eabbbaac7ecf7ff251ccb110bc1813d
Patrik <patsboy06@gmail.com>
2024-12-23 15:07:02 +0100

    elagazas bevezetese

--- main.js
+++ main.js
@@ -75,9 +75,7 @@ tr.appendChild(th_3); //A fejléc sorában lévő 3.cella hozzáadása a fejléc
 const tbody = document.createElement('tbody'); //Törzs létrehozása.
 table.appendChild(tbody); //Törzs hozzáadása a táblázathoz.
 
-for(let i = 0; i < array.length; i++) { //Ciklus segítségével végighaladás az arrayen.
-    const currentElement = array[i]; //Változó definiálása, amibe az array aktuális számú elemét belerakom.
-
+for(const currentElement of array){ //Végighaladás az arrayen, a currentElement az aktuális elem.
     const tr_1 = document.createElement('tr'); //Egy új sor létrehozása a táblázatba.
     tbody.appendChild(tr_1); //Az adott sor hozzáfűzése a táblázathoz.
 
@@ -93,14 +91,17 @@ for(let i = 0; i < array.length; i++) { //Ciklus segítségével végighaladás 
     hadero_1.innerHTML = currentElement.hadero_1; //A cella tartalmának megadása az aktuális elem haderőnek az értékével.
     tr_1.appendChild(hadero_1); //A létrehozott cellát hozzáadom a már korábban létrehozott sorhoz.
 
-    const tr_2 = document.createElement('tr'); //Még egy új sor létrehozása a táblázatba.
-    tbody.appendChild(tr_2); //Az adott sor hozzáfűzése a táblázathoz.
+    if(currentElement.felek_2 !== undefined && currentElement.hadero_2 !== undefined){ //Ha a currentElement.felek_2 és a currentElement.hadero_2 nem egyenlő undefineddal, akkor végig megy az elágazáson.
+        const tr_2 = document.createElement('tr'); //Még egy új sor létrehozása a táblázatba.
+        tbody.appendChild(tr_2); //Az adott sor hozzáfűzése a táblázathoz.
+        harc.rowSpan = 2; //Ha idáig lefut, akkor összevonjuk az harc 2 sorát.
 
-    const felek_2 = document.createElement('td'); //Új cella létrehozása az adott sorban.
-    felek_2.innerHTML = currentElement.felek_2; //A cella tartalmának megadása az aktuális elem feleknek az értékével.
-    tr_2.appendChild(felek_2); //A létrehozott cellát hozzáadom a már korábban létrehozott sorhoz.
+        const felek_2 = document.createElement('td'); //Új cella létrehozása az adott sorban.
+        felek_2.innerHTML = currentElement.felek_2; //A cella tartalmának megadása az aktuális elem feleknek az értékével.
+        tr_2.appendChild(felek_2); //A létrehozott cellát hozzáadom a már korábban létrehozott sorhoz.
 
-    const hadero_2 = document.createElement('td'); //Új cella létrehozása az adott sorban.
-    hadero_2.innerHTML = currentElement.hadero_2; //A cella tartalmának megadása az aktuális elem haderőnek az értékével.
-    tr_2.appendChild(hadero_2); //A létrehozott cellát hozzáadom a már korábban létrehozott sorhoz.
+        const hadero_2 = document.createElement('td'); //Új cella létrehozása az adott sorban.
+        hadero_2.innerHTML = currentElement.hadero_2; //A cella tartalmának megadása az aktuális elem haderőnek az értékével.
+        tr_2.appendChild(hadero_2); //A létrehozott cellát hozzáadom a már korábban létrehozott sorhoz.
+    }
 }
